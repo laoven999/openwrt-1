4a164a94bac62eab6df14552e24bc0ee203d23c8
laoven <155006849+laoven999@users.noreply.github.com>
2024-03-07 12:12:41 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,6 +9,8 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.0.50/g' package/base-files/files/bin/config_generate
