2e66fab47ad0e6cc7b179665e4fc7c32aeb2a107
laoven <155006849+laoven999@users.noreply.github.com>
2024-03-10 12:24:03 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,4 +17,7 @@
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git paswall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+sed -i '$a src-git pwneed https://github.com/xiaorouji/openwrt-passwall-packages.git' feeds.conf.default
+
+
